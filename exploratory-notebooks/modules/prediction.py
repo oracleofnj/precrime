2dba085238f8bada380c4c7511730aa50f503432
oracleofnj <jared.samet@aya.yale.edu>
2017-11-21 18:47:44 -0500

    Add sample model

--- exploratory-notebooks/modules/prediction.py
+++ exploratory-notebooks/modules/prediction.py
@@ -2,9 +2,11 @@
 import numpy as np
 import pandas as pd
 import datetime
+import csv
 from .weather import load_weather_data
 from .nypd_data import load_pivoted_felonies
 from .nyc_shapefiles import load_census_info
+from sklearn.linear_model import Ridge
 
 
 def load_all_data():
@@ -130,6 +132,7 @@ def create_test_period(first_date, last_date):
     test_times['TEST_DAY'] = test_times.index.get_level_values(0).day
     test_times['TEST_HOURGROUP'] = test_times.index.get_level_values(1)
     test_times.reset_index(inplace=True)
+    test_times.index.rename('Index', inplace=True)
     return test_times[[
         'TEST_YEAR', 'TEST_MONTH', 'TEST_DAY', 'TEST_HOURGROUP'
     ]]
@@ -186,3 +189,92 @@ def create_coarsegrained_split(n=5, random_state=4800):
             ignore_index=True
         )
     return test_times
+
+
+def create_2016_split():
+    """Create the entire year of 2016 as a test mask."""
+    first_date = datetime.date(2016, 1, 1)
+    last_date = datetime.date(2017, 1, 1)
+    return create_test_period(first_date, last_date)
+
+
+def save_splits(filepath='../precrime_data/'):
+    """Create the train/test splits and save them to disk."""
+    splits = {
+        'fine': create_finegrained_split().sort_index(),
+        'coarse': create_coarsegrained_split().sort_index(),
+        '2016': create_2016_split().sort_index(),
+    }
+    for k, v in splits.items():
+        v.to_csv(
+            filepath + 'split_{0}.csv'.format(k),
+            quoting=csv.QUOTE_NONNUMERIC
+        )
+
+
+def load_splits(filepath='../precrime_data/'):
+    """Load the saved train/test splits from disk."""
+    splits = ['fine', 'coarse', '2016']
+    return {
+        split: pd.read_csv(
+            filepath + 'split_{0}.csv'.format(split),
+            index_col=0
+        )
+        for split in splits
+    }
+
+
+def create_all_splits(crime_data, splits):
+    """Create X_train, X_test, y_train, y_test for each split in splits.
+
+    Parameters
+    ----------
+    crime_data : DataFrame
+        A DataFrame with columns 'COMPLAINT_YEAR', 'COMPLAINT_MONTH',
+        'COMPLAINT_DAY', and 'COMPLAINT_HOURGROUP', in the format returned by
+        load_all_data().
+
+    splits : dict
+        A dictionary of {split_name : DataFrame}, in the format returned by
+        load_splits().
+    """
+    return {
+        k: precrime_train_test_split(crime_data, v)
+        for k, v in splits.items()
+    }
+
+
+def sample_model(X_train, y_train, X_test):
+    """Example model to perform ridge regression on each felony type."""
+    def add_categorical_features(X):
+        X_features = X[[
+            'ADDR_PCT_CD',
+            'COMPLAINT_MONTH',
+            'temperature',
+            'precipIntensity'
+        ]].copy()
+        X_features['COMPLAINT_DAY_HOUR'] = \
+            X['COMPLAINT_DAYOFWEEK'].astype(str) + '_' + \
+            X['COMPLAINT_HOURGROUP'].astype(str)
+        return pd.get_dummies(
+            X_features,
+            columns=['ADDR_PCT_CD', 'COMPLAINT_MONTH', 'COMPLAINT_DAY_HOUR']
+        )
+
+    X_train_features = add_categorical_features(X_train)
+    X_test_features = add_categorical_features(X_test)
+    y_pred = X_test[[
+        'COMPLAINT_YEAR',
+        'COMPLAINT_MONTH',
+        'COMPLAINT_DAY',
+        'COMPLAINT_HOURGROUP',
+        'ADDR_PCT_CD'
+    ]].copy()
+    crime_types = y_train.select_dtypes(exclude=['object']).columns
+
+    for crime_type in crime_types:
+        ridge = Ridge()
+        ridge.fit(X_train_features, y_train[crime_type])
+        y_pred[crime_type] = ridge.predict(X_test_features)
+
+    return y_pred
