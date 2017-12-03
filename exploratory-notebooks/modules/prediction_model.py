79e9fbba07169c7ea868e949b9bba9fa0edf1757
Panpancheng <sweetolivepan@gmail.com>
2017-12-03 17:07:59 -0500

    Use RandomForestRegressor instead of RandomForestClassifier

--- exploratory-notebooks/modules/prediction_model.py
+++ exploratory-notebooks/modules/prediction_model.py
@@ -6,7 +6,9 @@ import pandas as pd
 import datetime
 import csv
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.naive_bayes import GaussianNB
 
 # Use MultinomialNB
 def sample_model_NB(X_train, y_train, X_test):
@@ -74,9 +76,46 @@ def sample_model_RF(X_train, y_train, X_test):
     crime_types = y_train.select_dtypes(exclude=['object']).columns
 
     for crime_type in crime_types:
-        clf = RandomForestClassifier()
+        #clf = RandomForestClassifier()
+        clf = RandomForestRegressor(max_depth=2, random_state=0)
         clf.fit(X_train_features, y_train[crime_type])
         y_pred[crime_type] = clf.predict(X_test_features)
 
     return y_pred
 
+
+# Use Guassian NB
+def sample_model_GNB(X_train, y_train, X_test):
+    """Example model to perform ridge regression on each felony type."""
+    def add_categorical_features(X):
+        X_features = X[[
+            'ADDR_PCT_CD',
+            'COMPLAINT_MONTH',
+            'temperature',
+            'precipIntensity'
+        ]].copy()
+        X_features['COMPLAINT_DAY_HOUR'] = \
+            X['COMPLAINT_DAYOFWEEK'].astype(str) + '_' + \
+            X['COMPLAINT_HOURGROUP'].astype(str)
+        return pd.get_dummies(
+            X_features,
+            columns=['ADDR_PCT_CD', 'COMPLAINT_MONTH', 'COMPLAINT_DAY_HOUR']
+        )
+
+    X_train_features = add_categorical_features(X_train)
+    X_test_features = add_categorical_features(X_test)
+    y_pred = X_test[[
+        'COMPLAINT_YEAR',
+        'COMPLAINT_MONTH',
+        'COMPLAINT_DAY',
+        'COMPLAINT_HOURGROUP',
+        'ADDR_PCT_CD'
+    ]].copy()
+    crime_types = y_train.select_dtypes(exclude=['object']).columns
+
+    for crime_type in crime_types:
+        clf = GaussianNB()
+        clf.fit(X_train_features, y_train[crime_type])
+        y_pred[crime_type] = clf.predict(X_test_features)
+
+    return y_pred
